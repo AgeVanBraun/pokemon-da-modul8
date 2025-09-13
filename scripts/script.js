7e6a1e91a8b8f6278fb3e6993330eea9c25e13fc
Heiko <heikobraun@AgeVanBraun>
2025-09-13 14:00:00 +0200

    basics and start fetchData

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let URL_BASE = "https://pokeapi.co/api/v2/pokemon";
+
+function init (){
+    console.log("Test");
+    fetchData()
+
+}
+
+async function fetchData() {
+  let response = await fetch(URL_BASE);
+  let responseToJson = await response.json();
+  console.log(responseToJson.results[0]);
+}
+
+function renderPokemonBox (){
+    
+}
