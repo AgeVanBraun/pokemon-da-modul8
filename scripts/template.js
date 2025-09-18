5868d2c6218713199b533db35b2d694758295af0
Heiko <heikobraun@AgeVanBraun>
2025-09-18 15:49:06 +0200

    updated design pokemons cards and button

--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,15 @@
 function getPokemonsTemplate(index, type1, type2) {
   return  `<div class="pokemon-box">
             <div class="pokemon-header">
-             <p>${pokemonList[index].name}</p>
+             <p>${pokemonList[index].name.toUpperCase()}</p>
              <p>#${[index + 1]}</p>
              </div>
             <div class="pokemon-img">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg" alt="">
             </div>
              <div class="pokemon-footer">
-            <p>${type1}</p>
-            <p>${type2}</p>
+            <p class="txt-shadow">${type1}</p>
+            <p class="txt-shadow">${type2}</p>
             </div>
             </div>`;
 }
